205fe8cec7ae4217a61cd05b1b84b5c6ac565e4d
aaronmivo <aamivo93@gmail.com>
2021-09-10 16:36:57 -0700

    Basic routing

---
+++ furrever/src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Favorites() {
+    return (
+        <div>
+            Favorite Pets
+        </div>
+    )
+}
+
+export default Favorites
---
+++ furrever/src/pages/PetDetails/PetDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PetDetails() {
+    return (
+        <div>
+            Details on a pet (Clicking from search page)
+        </div>
+    )
+}
+
+export default PetDetails
