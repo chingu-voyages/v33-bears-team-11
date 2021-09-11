6ba9ad699bdfff70acc9359029974d65779f5537
Jaime Alvarez <jrralvarez@gmail.com>
2021-09-11 15:50:49 -0500

    moved the main folder out of the furrever folder. updated the login page to accept information for an email and password. added a useForm file to handle our register and login page information

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+
+import Navbar from './components/Navbar/Navbar'
+
+//Page imports
+import Home from './pages/Home/Home';
+import About from './pages/About/About'
+import Login from './pages/Login/Login';
+import Search from './pages/Search/Search';
+import Favorites from './pages/Favorites/Favorites';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/search" component={Search}></Route>
+          <Route exact path="/favorites" component={Favorites}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
