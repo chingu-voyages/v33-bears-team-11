8c9858fff71a498e46d4291d54b674a35e57c015
aaronmivo <aamivo93@gmail.com>
2021-09-14 17:23:54 -0700

    Checking attributes

--- src/components/PetDetails/Info.tsx
+++ src/components/PetDetails/Info.tsx
@@ -1,13 +1,34 @@
+//@ts-nocheck
 import React from 'react'
 
+
 function Info({age, gender, size, species, attributes}) {
+    let attributeOps = [
+        "spayed_neutered",
+        "house_trained",
+        "declawed",
+        "special_needs",
+        "shots_current",
+        "children",
+        "dogs",
+        "cats"
+    ]
+
+    const checkAttributes =() => {
+        attributeOps = attributeOps.filter(val => !attributes.includes(val))
+    }
     return (
         <div>
+            {checkAttributes()}
             {age}
             {gender}
             {size}
             {species}
-            {attributes.join(", ")}
+            {/*true attributes */}
+            {attributes.join(", ")}  
+            {'\n'}
+            {/*false attributes */}
+            {attributeOps}
         </div>
     )
 }
